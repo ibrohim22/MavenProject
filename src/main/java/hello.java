2777e27f80b08b7cfbcd65a642f4546a756289de
ibrohim22 <9682228i@gmail.com>
2021-11-08 14:30:10 +0100

    update added more feature

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -2,5 +2,8 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing a new feature");
+        System.out.println("Local repo is published to github repo");
+        System.out.println("Adding more work");
+
     }
 }
